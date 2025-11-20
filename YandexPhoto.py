50194f8650b9b1567c04f63ea5999a48d29a267b
DANILEVICH <danila@MacBook-Pro-Danila.local>
2025-11-21 00:47:00 +0300

    new url

--- YandexPhoto.py
+++ YandexPhoto.py
@@ -399,23 +399,23 @@ def extract_image_url(item):
 base_url = "https://cloud-api.yandex.net/v1/disk/public/resources"
 
 urls = [
-    "https://tanyaname.ru/disk/19-09-2025-dilya-drvmfr",
-    "https://tanyaname.ru/disk/16-09-2025-tatyana-j4vq0d",
-    "https://tanyaname.ru/disk/29-06-2025-gleb-i-katerina-j7s5dw",
-    "https://tanyaname.ru/disk/zvezdy-vbdwmn",
-    "https://tanyaname.ru/disk/04-06-2025-viktoriya-tdjk96",
-    "https://tanyaname.ru/disk/1-maya-v9zqgr",
-    "https://tanyaname.ru/disk/14-03-2025-4-klass-sb38mz",
-    "https://tanyaname.ru/disk/16-02-2025-16-fevralya-dsswb0",
-    "https://tanyaname.ru/disk/12-02-2025-egor-i-tatyana-bq325h",
-    "https://tanyaname.ru/disk/09-02-2025-schuka-ltsqf7",
-    "https://tanyaname.ru/disk/08-12-2024-dlya-viki-z6gr4b",
-    "https://tanyaname.ru/disk/06-12-2024-6-dekabrya-krh1xt",
-    "https://tanyaname.ru/disk/22-11-2024-svet-rodnoy-pesni-q578fc",
-    "https://tanyaname.ru/disk/03-11-2024-anya-mj3j0v",
-    "https://tanyaname.ru/disk/18-10-2024-vera-46gpb2",
-    "https://tanyaname.ru/disk/16-09-2024-siblings-j72sbf",
-    "https://tanyaname.ru/disk/14-09-2024-fandom-fest-p7z5x8",
+    "https://tanyaname.ru/disk/19-09-2025-dilya-drvmfr/pieces?design_variant=masonry&folder_path=photos",
+    "https://tanyaname.ru/disk/16-09-2025-tatyana-j4vq0d/pieces?design_variant=masonry&folder_path=photos",
+    "https://tanyaname.ru/disk/29-06-2025-gleb-i-katerina-j7s5dw/pieces?design_variant=masonry&folder_path=1",
+    "https://tanyaname.ru/disk/zvezdy-vbdwmn/pieces?design_variant=masonry&folder_path=photos",
+    "https://tanyaname.ru/disk/04-06-2025-viktoriya-tdjk96/pieces?design_variant=masonry&folder_path=foto",
+    "https://tanyaname.ru/disk/1-maya-v9zqgr/pieces?design_variant=masonry&folder_path=photos",
+    "https://tanyaname.ru/disk/14-03-2025-4-klass-sb38mz/pieces?design_variant=masonry&folder_path=photos",
+    "https://tanyaname.ru/disk/16-02-2025-16-fevralya-dsswb0/pieces?design_variant=masonry&folder_path=1",
+    "https://tanyaname.ru/disk/12-02-2025-egor-i-tatyana-bq325h/pieces?design_variant=masonry&folder_path=photos",
+    "https://tanyaname.ru/disk/09-02-2025-schuka-ltsqf7/pieces?design_variant=masonry&folder_path=photos",
+    "https://tanyaname.ru/disk/08-12-2024-dlya-viki-z6gr4b/pieces?design_variant=masonry&folder_path=photos",
+    "https://tanyaname.ru/disk/06-12-2024-6-dekabrya-krh1xt/pieces?design_variant=masonry&folder_path=photos",
+    "https://tanyaname.ru/disk/22-11-2024-svet-rodnoy-pesni-q578fc/pieces?design_variant=masonry&folder_path=photos-1",
+    "https://tanyaname.ru/disk/03-11-2024-anya-mj3j0v/pieces?design_variant=masonry&folder_path=anya",
+    "https://tanyaname.ru/disk/18-10-2024-vera-46gpb2/pieces?design_variant=masonry&folder_path=photos",
+    "https://tanyaname.ru/disk/16-09-2024-siblings-j72sbf/pieces?design_variant=masonry&folder_path=photos",
+    "https://tanyaname.ru/disk/14-09-2024-fandom-fest-p7z5x8/pieces?design_variant=masonry&folder_path=photos",
     "https://disk.yandex.ru/d/NMxM1lo88n8AEw",
     "https://disk.yandex.ru/d/hAebMUXRgEgdrg",
     "https://disk.yandex.ru/d/tSrb-uZU9AQjSg",
@@ -492,7 +492,7 @@ def fetch_tanyaname_folder(url):
     """Парсит страницу tanyaname.ru/disk/... и собирает ссылки изображений."""
     collected = []
     try:
-        resp = requests.get(url + "/pieces?design_variant=masonry&folder_path=photos", timeout=15)
+        resp = requests.get(url + "", timeout=15)
         resp.raise_for_status()
         soup = BeautifulSoup(resp.text, "html.parser")
         for a in soup.find_all("a", {"data-role": "gallery-link"}):
